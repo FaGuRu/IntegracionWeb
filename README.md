b2f7e990471d2cf8825852015d75b030dad4ca9f
FaGuRu <193247@ids.upchiapas.edu.mx>
2020-10-02 17:33:56 -0500

    Project cut 1 finished

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Proyecto terminado
+Se agrego la implementacion de un login con Google con AngularFire, la pagina de Login,Landing-page y registro funcionan perfectamente.
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   mandar : boolean = false;
-  
-//private afAuth: AngularFireAuth 
+  pasa : String ='';
   constructor(private router: Router, private afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
@@ -31,24 +30,12 @@ export class LoginComponent implements OnInit {
     var provider = new firebase.auth.GoogleAuthProvider();
     console.log('entra');
     
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      //var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-    
-    
+    firebase.auth().signInWithPopup(provider).then(res => {
+      console.log('Successfully logged in!')
+      this.router.navigate(['/landing-page'])
+    }).catch(error => {
+      console.log(error)
+  });
     
   }
 
